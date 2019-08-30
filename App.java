c334249732fd0ab9e0d357edbe201621b1b23bdb
Willian S. Dias <willian.dias@edu.pucrs.br>
2019-08-30 20:22:07 -0300

    Alterações iniciais

--- App.java
+++ App.java
@@ -1,9 +1,17 @@
+/*
+ *  T1 - Projeto e Otimização de Algoritmos
+ *
+ *  Willian Schmiele Dias
+ *  2019/2
+ *
+ */
+
 public class App{
     public static void main (String[] args){
 
         if (args.length == 2){
             //System.out.println(args[0]+" ; "+args[1]);
-            System.out.println(Karatsuba.Multiplicar(args[0], args[1]));
+            System.out.println(Karatsuba.KMult(args[0], args[1]));
         }
         else System.out.println("Inválido! Formato: java App arg1 arg2");
     }
--- Karatsuba.java
+++ Karatsuba.java
@@ -1,62 +1,76 @@
+/*
+ *  T1 - Projeto e Otimização de Algoritmos
+ *
+ *  Willian Schmiele Dias
+ *  2019/2
+ *
+ */
+
 public class Karatsuba{
 
-    public Karatsuba(){
+    public Karatsuba(){}
 
-    }
-
-    public static String Multiplicar(String a, String b){
-
-        //System.out.println(a.length());
-        //System.out.println(b.length());
-        return KMult(a, b);
-    }
-
-    private static String KMult(String a, String b){
-
+    public static String KMult(String a, String b){
+    
         if (a.length() == 1 && b.length() == 1)
             return String.valueOf(Integer.valueOf(a) * Integer.valueOf(b));
-        else{
+        else
+        {
+            if (a.length() > b.length())
+            {
+                while (a.length() % 2 != 0) a = "0" + a;
+                while (b.length() < a.length()) b = "0" + b;
+            }
+            else
+            {
+                while (b.length() % 2 != 0) b = "0" + b;
+                while (a.length() < b.length()) a = "0" + a;
+            }
 
+            System.out.println(a+" ; "+b);
             String a1 = a.substring(0, a.length()/2);
             String a2 = a.substring(a.length()/2, a.length());
             String b1 = b.substring(0, b.length()/2);
             String b2 = b.substring(b.length()/2, b.length());
 
-            //System.out.println(a1+" ; "+a2+" | "+b1+" ; "+b2);
+            System.out.println(a1+" ; "+a2+" | "+b1+" ; "+b2);
+        
+            String shift = "";
+            while (shift.length() < a.length()/2) shift += "0";
+            System.out.println(shift);
 
-            return KAdd(KMult(a1, b1), KMult(a1, b2), KMult(a2, b1), KMult(a2, b2));
+            //return ""; //KAdd(KMult(a1, b1), KMult(a1, b2), KMult(a2, b1), KMult(a2, b2));
+
+            //return KAdd (KMult(a1, b1) + shift + shift, KAdd (KAdd (KMult(a1, b2), KMult(a2, b1)) + shift, KMult(a2, b2)));
         }
     }
 
-    private static String KAdd(String a, String b, String c, String d){
+    //private static String KAdd(String a, String b, String c, String d){
+    private static String KAdd(String a, String b){
 
-        int i, limit, aux, carry;
-        char[] x, y, z, w;
+        boolean carry;
+        int aux;
 
-        limit = 2 * a.length(); for (i = 0; i < limit; i++) a += "0";
-        limit = b.length(); for (i = 0; i < limit; i++) b += "0";
-        limit = c.length(); for (i = 0; i < limit; i++) c += "0";
-
-        limit = a.length() - b.length(); for (i = 0; i < limit; i++) b = "0" + b;
-        limit = a.length() - c.length(); for (i = 0; i < limit; i++) c = "0" + c;
-        limit = a.length() - d.length(); for (i = 0; i < limit; i++) d = "0" + d;
-
-        //System.out.println("a: "+a);
-        //System.out.println("b: "+b);
-        //System.out.println("c: "+c);
-        //System.out.println("d: "+d);
-
-        x = a.toCharArray();
-        y = b.toCharArray();
-        z = c.toCharArray();
-        w = d.toCharArray();
-
-        for (i = x.length - 1; i >= 0; i--){
-            aux = (x[i]-48) + (y[i]-48) + (z[i]-48) + (w[i]-48);
-			carry = aux > 9 ? 1 : 0;
-			x[i] = (char)(carry == 0 ? aux : );
+        if (a.length() > b.length())
+        {
+            while (b.length() < a.length()) b = "0" + b;
+            char[] aChar = s.toCharArray(), bChar = b.toCharArray(), res = new char[a.length()];
+            for (int index = res.length - 1; index > 0; i--)
+            {
+                aux = (aChar[index]-48) + (bChar[index]-48)
+                if (aux < 10)
+                    res[index] = (char)(aux + 48);
+                else
+                {
+                    res[index] = (char)(aux - 10 + 48);
+                    carry = true;
+                }
+            }
         }
+        else
+        {
 
-        return String.valueOf(x);
+        }
+        return "";
     }
 }
